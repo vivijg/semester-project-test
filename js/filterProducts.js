9265c003c5a3bedcd16334ac08542ad79a3648f9
vivijg <57644737+vivijg@users.noreply.github.com>
2020-11-11 18:17:25 +0100

    Add files via upload

--- js/filterProducts.js
+++ js/filterProducts.js
@@ -4,20 +4,19 @@ import { displayMessage } from "./displayMessage.js";
 export function filterProducts(products) {
   const searchInput = document.querySelector(".search");
 
-  // attach onkeyup listener to the searchInput.
+  
   searchInput.onkeyup = function(event) {
-    // save the user input value and trim off any whitespaces
+    
     const searchValue = event.target.value.trim();
 
     const filteredPrices = products.filter(function(product) {
-      // use parseFloat() method to convert the string/text to number, and keep decimals
-      // we also check if searchValue is empty so that all products show if nothing is entered.
+    
       if (parseFloat(product.price) <= searchValue || searchValue === "") {
         return true;
       }
     });
 
-    // check length of filteredPrices array to determine if results or error should be shown.
+   
     if (filteredPrices.length > 0) {
       renderProducts(filteredPrices);
     } else {
